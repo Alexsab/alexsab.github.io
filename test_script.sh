542237f9f9226ce3492a5a9d9dc022e570d65f56
Alexander Sabanov <alexsab@mail.ru>
2023-07-11 18:12:08 +0400

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,9 +1,11 @@
-if [ "68d5e43fc27fbb3e96d167312ca0ac01be6dfe83" == "0000000000000000000000000000000000000000" ]; then
+if [ "fff10e3e2104a741168da6ec3c90b423e08fa767" == "0000000000000000000000000000000000000000" ]; then
   git fetch origin master
   git checkout master
-  COMMITS=$(git log --pretty=format:"'<code>%h</code>' - %an, %ar : %s" 244842f01f2de06e5cbf1814a79c0763f1b380d6..origin)
+  COMMITS=$(git log --pretty=format:"'<code>%h</code>' - %an, %ar : %s" master..f002985dd1253cef0c8dd98806116eda8a0ca1f2)
+  echo "before=f002985dd1253cef0c8dd98806116eda8a0ca1f2"
 else
-  COMMITS=$(git log --pretty=format:"'<code>%h</code>' - %an, %ar : %s" 68d5e43..b839b95)
+  COMMITS=$(git log --pretty=format:"'<code>%h</code>' - %an, %ar : %s" fff10e3e2104a741168da6ec3c90b423e08fa767..f002985dd1253cef0c8dd98806116eda8a0ca1f2)
+  echo "before=fff10e3e2104a741168da6ec3c90b423e08fa767..f002985dd1253cef0c8dd98806116eda8a0ca1f2"
 fi
 
 # Convert the list of commits to an array
