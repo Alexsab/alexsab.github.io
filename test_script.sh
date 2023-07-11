a4c5bfc5c4b06a5d2480c3bcb3858e9fe5c1502e
Alexander Sabanov <alexsab@mail.ru>
2023-07-11 21:29:49 +0400

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,19 +1,17 @@
 if [ "fff10e3e2104a741168da6ec3c90b423e08fa767" == "0000000000000000000000000000000000000000" ]; then
   git fetch origin master
   git checkout master
-  COMMITS=$(git log --pretty=format:"'<code>%h</code>' - %an, %ar : %s" master..f002985dd1253cef0c8dd98806116eda8a0ca1f2)
+  mapfile -t COMMITS_ARRAY < <(git log --pretty=format:"'<code>%h</code>' - %an, %ar : %s" master..f002985dd1253cef0c8dd98806116eda8a0ca1f2)
   echo "before=f002985dd1253cef0c8dd98806116eda8a0ca1f2"
 else
-  COMMITS=$(git log --pretty=format:"'<code>%h</code>' - %an, %ar : %s" fff10e3e2104a741168da6ec3c90b423e08fa767..f002985dd1253cef0c8dd98806116eda8a0ca1f2)
+  mapfile -t COMMITS_ARRAY < <(git log --pretty=format:"'<code>%h</code>' - %an, %ar : %s" fff10e3e2104a741168da6ec3c90b423e08fa767..f002985dd1253cef0c8dd98806116eda8a0ca1f2)
   echo "before=fff10e3e2104a741168da6ec3c90b423e08fa767..f002985dd1253cef0c8dd98806116eda8a0ca1f2"
 fi
 
-# Convert the list of commits to an array
-IFS=$'\n' read -rd '' -a COMMITS_ARRAY <<< "$COMMITS"
+echo "${COMMITS_ARRAY[@]}"
 
-# Process each commit
 echo "full_list<<EOF"
-echo "$COMMITS" | while read -r COMMIT; do
+for COMMIT in "${COMMITS_ARRAY[@]}"; do
   echo "$COMMIT"
 done
 echo "EOF"
